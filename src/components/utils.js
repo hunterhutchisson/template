8ad5a7ae1a310a855992c5dde8493f09c78d618b
Hunter Hutchisson <hunter.hutchisson@gmail.com>
2022-01-02 15:50:56 -0600

    key

--- src/components/utils.js
+++ src/components/utils.js
@@ -2,7 +2,7 @@ export const fetchHTML = async (markdown, setHtmlOutput) => {
     let result = (await fetch('https://api.github.com/markdown', {
         method: 'POST',
         headers: {
-            'authorization': "token " + "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",
+            'authorization': "token " + process.env.REACT_APP_MYKEY,
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({'mode': 'markdown', 'text': markdown})
